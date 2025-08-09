d98a0210b8a1c9e6a8b65422275f1c8d8eeb7c9e
Artur Moises <arturmoiscontato@gmail.com>
2025-08-09 17:10:43 -0300

    feat: enhance sign-in form with success and error notifications; implement user feedback for login errors

--- src/app/auth/components/sign-in-form.tsx
+++ src/app/auth/components/sign-in-form.tsx
@@ -23,6 +23,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   email: z.email("Email inválido!"),
@@ -34,6 +36,7 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>;
 
 const SignInForm = () => {
+  const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -44,11 +47,35 @@ const SignInForm = () => {
 
   async function onSubmit(values: FormValues) {
     try {
-      const { data, error } = await authClient.signIn.email({
+      await authClient.signIn.email({
         email: values.email,
         password: values.password,
         rememberMe: true,
         callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Login realizado com sucesso!");
+            router.push("/");
+          },
+          onError: (error) => {
+            if (error.error.code === "USER_NOT_FOUND") {
+              toast.error("Usuário não encontrado!");
+              return form.setError("email", {
+                message: "Usuário não encontrado!",
+              });
+            }
+            if (error.error.code === "INVALID_EMAIL_OR_PASSWORD") {
+              toast.error("Email ou senha inválidos!");
+              form.setError("email", {
+                message: "Email ou senha inválidos!",
+              });
+              return form.setError("password", {
+                message: "Email ou senha inválidos!",
+              });
+            }
+            toast.error(error.error.message);
+          },
+        },
       });
     } catch (error) {
       console.error(error);
