6487d587421e6a864721fbcab81c2f0117c5b2ea
Artur Moises <arturmoiscontato@gmail.com>
2025-08-09 18:37:06 -0300

    feat: add banner images to homepage and integrate them into the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+import Image from "next/image";
+
 import { Header } from "@/components/common/header";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="space-y-6 px-4">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+        <Image
+          src="/banner-02.png"
+          alt="Seja autentico"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 };
