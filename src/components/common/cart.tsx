ff87c8c94c5fe738f9fe61641d1246c8c55962ca
Artur Moises <arturmoiscontato@gmail.com>
2025-08-11 15:11:58 -0300

    feat: integrate React Query for cart management and enhance product actions with add-to-cart functionality

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,9 +1,23 @@
-import { ShoppingBasketIcon } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import { Loader2, ShoppingBasketIcon } from "lucide-react";
+import Image from "next/image";
+
+import { getCart } from "@/actions/get-cart";
 
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
 
 const Cart = () => {
+  const { data: cart, isPending: cartIsLoading } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () => getCart(),
+  });
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -12,7 +26,26 @@ const Cart = () => {
         </Button>
       </SheetTrigger>
       <SheetContent>
-
+        <SheetHeader>
+          <SheetTitle>Carrinho</SheetTitle>
+        </SheetHeader>
+        <div className="flex flex-col gap-4">
+          {cartIsLoading && (
+            <div className="flex items-center justify-center">
+              <Loader2 className="animate-spin" />
+            </div>
+          )}
+          {cart?.items.map((item) => (
+            <div key={item.id}>
+              <Image
+                src={item.productVariant.imageUrl}
+                alt={item.productVariant.product.name}
+                width={100}
+                height={100}
+              />
+            </div>
+          ))}
+        </div>
       </SheetContent>
     </Sheet>
   );
